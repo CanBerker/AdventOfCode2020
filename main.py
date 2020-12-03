9de064babfe1f98ca6d8f196aeab4f76bd604b43
canberker <canberkerwork@gmail.com>
2020-12-03 16:39:54 +0100

    feature: Solve day2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from day1 import *
+from day2 import *
 
 if __name__ == '__main__':
     # https://adventofcode.com/2020
 
-    print(f"day1-part1 result: {day1_part1(day1_input)}")
-    print(f"day1-part2 result: {day1_part2(day1_input)}")
+    # print(f"day1-part1 result: {day1_part1(day1_input)}")
+    # print(f"day1-part2 result: {day1_part2(day1_input)}")
+
+    print(f"day2-part1 result: {day2_part1(day2_input)}")
+    print(f"day2-part2 result: {day2_part2(day2_input)}")
