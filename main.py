7a2009b9bf86bc6224add7728a50dd3d2abe65f8
canberker <canberkerwork@gmail.com>
2020-12-03 16:03:15 +0100

    feature: Initial commit with day 1

---
+++ main.py
@@ -0,0 +1,7 @@
+from day1 import *
+
+if __name__ == '__main__':
+    # https://adventofcode.com/2020
+
+    print(f"day1-part1 result: {day1_part1(day1_input)}")
+    print(f"day1-part2 result: {day1_part2(day1_input)}")
