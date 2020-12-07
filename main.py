e2179b2b001655c3f63d1d26455f42d2c76a6979
canberker <canberkerwork@gmail.com>
2020-12-07 21:42:36 +0100

    feature: solve day 3

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from day1 import *
-from day2 import *
-
 if __name__ == '__main__':
     # https://adventofcode.com/2020
 
+    # from day1 import *
     # print(f"day1-part1 result: {day1_part1(day1_input)}")
     # print(f"day1-part2 result: {day1_part2(day1_input)}")
 
-    print(f"day2-part1 result: {day2_part1(day2_input)}")
-    print(f"day2-part2 result: {day2_part2(day2_input)}")
+    # from day2 import *
+    # print(f"day2-part1 result: {day2_part1(day2_input)}")
+    # print(f"day2-part2 result: {day2_part2(day2_input)}")
+
+    # from day3 import *
+    # right_amount, down_amount = 3, 1
+    # print(f"day3-part1 result: {day3_part1(day3_input, right_amount, down_amount)}")
+    # amounts = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    # print(f"day3-part1 result: {day3_part2(day3_input, amounts)}")
+
+    
+
